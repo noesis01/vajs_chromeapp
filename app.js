0edb529cabdcba3bf6a25e7093b8242389e3db55
noesis01 <eupharia19@gmail.com>
2023-08-22 17:18:35 +0900

    momentum_1

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const age = 96;
-function calcuKrAge(ageOfF) {
-    return ageOfF + 2;
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting")
+
+function onLoginSubmit(event) {
+    const HIDDEN_CLASSNAME = "hidden";
+    event.preventDefault();
+    const username = loginInput.value;
+    localStorage.setItem("username", username);
+
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    
+    greeting.innerText = `Hello, ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
 }
+
+loginForm.addEventListener("submit", onLoginSubmit);
