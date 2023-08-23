f53d05fe401edf91b1f5d9d3980b527b3666e157
noesis01 <eupharia19@gmail.com>
2023-08-23 15:49:24 +0900

    done!

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+const API_KEY = "550bef17880496dc9719ee5a36bf9000";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
+    fetch(url).then((response) => response.json()).then((data) => {
+        const nowWeather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("weather span:last-child");
+
+        const name = data.name;
+        const weather = data.weather[0].main;
+        
+        city.innerText = name;
+        nowWeather.innerText = weather;
+    });   
+
+}
+
+function onGeoError() {
+    alert("Can't locate you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
